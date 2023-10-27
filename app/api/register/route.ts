9e58eccfee1255530b0053c7e391bcf487a39c2a
Jerome Brule <jerome.brule@gmail.com>
2023-10-27 14:00:40 -0700

    user saved in db

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "prisma/client";
-import { boolean, z } from "zod";
+import { z } from "zod";
 import bcrypt from "bcrypt";
 
 const schema = z.object({
